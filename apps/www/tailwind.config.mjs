b8a7dc30305c5b4c58d321327d387d9371779a10
HyperionU <108021531+HyperionU@users.noreply.github.com>
2025-01-11 05:54:25 +0000

    build(www): add hero-patterns

--- apps/www/tailwind.config.mjs
+++ apps/www/tailwind.config.mjs
@@ -53,5 +53,5 @@ export default {
     		}
     	}
     },
-	plugins: [require("tailwindcss-animate")],
+	plugins: [require("tailwindcss-animate"), require("@lorenzopalaia/tailwind-hero-patterns")],
 }
