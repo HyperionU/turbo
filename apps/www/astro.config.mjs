24e2a23d023784f796a18a241af551eba3a554b8
HyperionU <108021531+HyperionU@users.noreply.github.com>
2025-05-19 20:46:03 +0000

    build: update packages & lockfile

--- apps/www/astro.config.mjs
+++ apps/www/astro.config.mjs
@@ -2,16 +2,16 @@
 import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
-import tailwind from '@astrojs/tailwind';
 
 import icon from 'astro-icon';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind({
-    applyBaseStyles: false
-  }), icon()],
-  experimental: {
-    svg: true
-  }
+  integrations: [react(), icon()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
